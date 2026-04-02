5db1eb9bd488bda0352b2c058e4b8201908e7353
aysenurulkucreates <aysenurulku17@gmail.com>
2026-04-02 13:32:27 +0300

    Add: reverse-a-linked-list - submission-0

---
+++ Data Structures & Algorithms/reverse-a-linked-list/submission-0.js
@@ -0,0 +1,17 @@
+class Solution {
+    reverseList(head) {
+        let prev = null;
+        let curr = head;
+
+        while(curr !== null) {
+            let nextTemp = curr.next;
+
+            curr.next = prev;
+
+            prev = curr;
+
+            curr = nextTemp;
+        }
+        return prev;
+    }
+}
