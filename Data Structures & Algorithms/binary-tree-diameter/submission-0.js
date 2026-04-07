3f3ef5942dc701ba3925712c1e52072baa2090c1
aysenurulkucreates <aysenurulku17@gmail.com>
2026-04-07 20:04:25 +0300

    Add: binary-tree-diameter - submission-0

---
+++ Data Structures & Algorithms/binary-tree-diameter/submission-0.js
@@ -0,0 +1,21 @@
+
+class Solution {
+    diameterOfBinaryTree(root) {
+        let maxDiameter = 0;
+
+        function dfs(node) {
+          if(!node) return 0;
+
+          let leftDepth = dfs(node.left);
+          let rightDepth = dfs(node.right);
+
+          maxDiameter = Math.max(maxDiameter, leftDepth + rightDepth);
+
+          return Math.max(leftDepth, rightDepth ) + 1;
+        }
+        dfs(root);
+
+        return maxDiameter;
+    } 
+    
+}
