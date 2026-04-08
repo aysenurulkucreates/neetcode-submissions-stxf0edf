9bcb8c8e990343f07c75785ce2671c5aef49bf84
aysenurulkucreates <aysenurulku17@gmail.com>
2026-04-08 19:00:16 +0300

    Add: lowest-common-ancestor-in-binary-search-tree - submission-3

---
+++ Data Structures & Algorithms/lowest-common-ancestor-in-binary-search-tree/submission-3.js
@@ -0,0 +1,10 @@
+
+class Solution {
+    lowestCommonAncestor(root, p, q) {
+        if(root.val > p.val && root.val > q.val) return this.lowestCommonAncestor(root.left, p, q);
+        if(root.val < p.val && root.val < q.val) return this.lowestCommonAncestor(root.right, p, q);
+
+        return root;
+        
+}
+}
