ada80079cb62c8e9c845e14cd9a91487b29acf2e
aysenurulkucreates <aysenurulku17@gmail.com>
2026-04-13 10:37:20 +0300

    Add: combination-target-sum - submission-3

---
+++ Data Structures & Algorithms/combination-target-sum/submission-3.js
@@ -0,0 +1,24 @@
+class Solution {
+    combinationSum(nums, target) {
+        let res = [];
+         
+         function dfs(i, subset, total) {
+            if(total === target) {
+                res.push([...subset]);
+                return;
+            }
+            
+            if(total > target) return;
+
+            if(i >= nums.length) return;
+
+            subset.push(nums[i]);
+            dfs(i, subset, total+nums[i]);
+
+            subset.pop();
+            dfs(i+1, subset, total);
+         }
+         dfs(0, [], 0);
+         return res;
+    }
+}
