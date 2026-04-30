36379c96651e5d529ae93b60385d0b822b1ffbbd
aysenurulkucreates <aysenurulku17@gmail.com>
2026-04-30 14:36:00 +0300

    Add: number-of-one-bits - submission-0

---
+++ Data Structures & Algorithms/number-of-one-bits/submission-0.js
@@ -0,0 +1,12 @@
+class Solution {
+    hammingWeight(n) {
+        let count = 0;
+
+        while(n !== 0) {
+            n = n & (n - 1);
+
+            count++;
+        }
+        return count;
+    }
+}
