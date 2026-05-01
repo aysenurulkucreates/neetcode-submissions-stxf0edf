2f1ae508e3e674bce8546896173318cdb47aed16
aysenurulkucreates <aysenurulku17@gmail.com>
2026-05-01 09:53:05 +0300

    Add: missing-number - submission-1

---
+++ Data Structures & Algorithms/missing-number/submission-1.js
@@ -0,0 +1,13 @@
+class Solution {
+    missingNumber(nums) {
+        const n = nums.length;
+
+        const expectedSum = (n * (n + 1)) / 2;
+        let actualSum = 0;
+
+        for (let i = 0; i < n; i++) {
+            actualSum += nums[i]; 
+        }
+        return expectedSum - actualSum;
+    }
+}
