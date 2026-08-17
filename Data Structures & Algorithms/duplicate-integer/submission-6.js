783e8d0505395af044ba9feb37991c34bc1b528f
aysenurulkucreates <aysenurulku17@gmail.com>
2026-08-17 08:19:59 +0300

    Add: duplicate-integer - submission-6

---
+++ Data Structures & Algorithms/duplicate-integer/submission-6.js
@@ -0,0 +1,13 @@
+
+class Solution {
+    hasDuplicate(nums) {
+        const seen = new Set();
+
+        for(let i = 0; i < nums.length; i++){
+          if(seen.has(nums[i])) return true;
+
+          seen.add(nums[i]);
+        }
+        return false;
+    }
+}
